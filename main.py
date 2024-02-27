61d728443b6ab5e3a3841ad5506a79934d3233df
stungo12 <123669479+stungo12@users.noreply.github.com>
2024-02-27 11:25:55 -0600

    Made a semi-working program, though it only has an ADD function

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ cur = conn.cursor()
 # Create Table if none exists
 cur.execute("""CREATE TABLE IF NOT EXISTS recorder(
             recorder_id INT PRIMARY KEY,
-            month DATE,
+            month INT,
             day INT,
             year INT,
             hours REAL,
@@ -25,9 +25,50 @@ conn.commit()
 #   List, retrieve, edit, and delete functions
 #   Function to input 
 class Entry():
-    def __init__(self, entry) -> None:
-        self.entry = entry
+    def __init__(self, id, month, day, year, time, notes):
+        self.id = id
+        self.month = month
+        self.day = day
+        self.year = year
+        self.time = time
+        self.notes = notes
 
+    def add(self):
+        print("ADD")
+
+        # Have the user input their time log information into the system
+        self.id = int(input("Recorder ID: "))
+        self.month = int(input("Month: "))
+        self.day = int(input("Day: "))
+        self.year = int(input("Year: "))
+        self.time = int(input("Amount of time spent programming: "))
+
+        # Take the user input and add it to the database
+        cur.execute(f"""INSERT INTO recorder(recorder_id, month, day, year, time, notes)
+                    VALUES ({self.id}, {self.month}, {self.day}, {self.year}, {self.time}, {self.notes})
+
+""")
+        
+# Add function
+def add():
+        print("ADD")
+
+        # Have the user input their time log information into the system
+        id = int(input("Recorder ID: "))
+        month = int(input("Month: "))
+        day = int(input("Day: "))
+        year = int(input("Year: "))
+        time = int(input("Amount of time spent programming (in hours): "))
+        aldi = input("Did you work at Aldi? If so, till when? ")
+        afton = input("Do you work Afton Alps this afternoon? If so, then when will you leave? ")
+
+        # Take the user input and add it to the database
+        cur.execute("""INSERT INTO recorder(month, day, year, hours, aldi, afton)
+                    VALUES (month, day, year, time, aldi, afton)
+
+        """)
+        conn.commit()
+    
 
 # Function to show record entry page
 
@@ -41,6 +82,7 @@ def menu():
     print("(2) View all entries")
     print("(3) Edit an entry")
     print("(4) Delete an entry")
+    print("(-99) Quit")
 
     entry = int(input("What say you? "))
     return entry
@@ -54,7 +96,14 @@ def startPage():
 def main():
     startPage()
     entry = menu()
-    print(entry)
+    while (entry != -99):
+        if (entry == 1):
+            add()
+        elif (entry == -99):
+            break
+        
+        entry = menu()
+    
 
 if __name__ == '__main__':
     main()
