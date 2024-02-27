88f67b44568508b3afb04665ac073f0f4cba3f4d
stungo12 <123669479+stungo12@users.noreply.github.com>
2024-02-27 09:30:44 -0600

    Created an outline and functions/classes for Main.py and moved TextBase to a different folder so that the world doesn't see my programming habits (though that still may happen as the database will also be saved :( )

--- main.py
+++ main.py
@@ -18,3 +18,43 @@ cur.execute("""CREATE TABLE IF NOT EXISTS recorder(
 """)
 
 conn.commit()
+
+# Class for adding a new entry into the database
+#   Default Constructor
+#   User Constructor
+#   List, retrieve, edit, and delete functions
+#   Function to input 
+class Entry():
+    def __init__(self, entry) -> None:
+        self.entry = entry
+
+
+# Function to show record entry page
+
+# Function to show database, maybe only last 10-20 entries
+
+# Menu function
+def menu():
+    print("What would you like to do?")
+    print()
+    print("(1) Add new entry")
+    print("(2) View all entries")
+    print("(3) Edit an entry")
+    print("(4) Delete an entry")
+
+    entry = int(input("What say you? "))
+    return entry
+
+# Start page
+def startPage():
+    print("Welcome to your Programming Recorder. Here you can input how much you coded for the day and we will keep track for you.")
+    print()
+
+# Main function
+def main():
+    startPage()
+    entry = menu()
+    print(entry)
+
+if __name__ == '__main__':
+    main()
